47e14a643e73add71ee882564adfa600f97d3d3a
Mathieu Jobin <majobin@mdsol.com>
2021-12-09 11:26:07 +0900

    day3 part1 was easy, but part 2 is broken, commit current state before refactoring around

--- adventofcode2021.rb
+++ adventofcode2021.rb
@@ -99,28 +99,106 @@ def day2(part)
   puts "Answer: #{calculation[:forward] * calculation[:depth]}"
 end
 
-def day3(part)
-  bits_collector = File.read('input-day3.txt').split("\n").each_with_object({}) do |line, acc|
-    line.chars.each_with_index do |c, i|
-      acc[i] ||= []
-      acc[i] << c
+class Day3Solver
+  def data
+    @data ||= File.read('input-day3.txt').split("\n")
+  end
+
+  def bits_collector
+    bits_collector ||= data.each_with_object({}) do |line, acc|
+      line.chars.each_with_index do |c, i|
+        acc[i] ||= []
+        acc[i] << c
+      end
     end
   end
 
-  result = bits_collector.each_with_object(gamma: "", epsylon: "") do |(pos, values), acc|
-    # puts pos.inspect
-    counts = values.group_by(&:itself).map { |k, v| [k, v.length] }.to_h
-    if counts["0"] > counts["1"]
-      acc[:gamma] << "0"
-      acc[:epsylon] << "1"
-    else
-      acc[:gamma] << "1"
-      acc[:epsylon] << "0"
-    end
-    acc
+  def count_unique_values(values)
+    values.group_by(&:itself).map { |k, v| [k, v.length] }.to_h
   end
-  puts result.inspect
-  puts "Answer: #{result[:gamma].to_i(2) * result[:epsylon].to_i(2)}"
+
+  def solve_part1
+    result = bits_collector.each_with_object(gamma: "", epsylon: "") do |(pos, values), acc|
+      # puts pos.inspect
+      counts = count_unique_values(values)
+      if counts["0"] > counts["1"]
+        acc[:gamma] << "0"
+        acc[:epsylon] << "1"
+      else
+        acc[:gamma] << "1"
+        acc[:epsylon] << "0"
+      end
+      acc
+    end
+    puts result.inspect
+    puts "Answer: #{result[:gamma].to_i(2) * result[:epsylon].to_i(2)}"
+  end
+
+  def indexes_with_value(collection, value)
+    collection.each_with_index.select { |v, i| v == value }.map(&:last)
+  end
+
+  def found_value?(indexes)
+    indexes.is_a?(Array) && indexes.length == 1
+  end
+
+  def values_at(values, indexes)
+    # maybe use values_at instead of [] if its a problem
+    if indexes.is_a?(Array)
+      values.values_at *indexes
+    elsif indexes.is_a?(Range)
+      values[indexes]
+    else
+      raise "error"
+    end
+  end
+
+  def locate_indexes(values, acc, rating, condition)
+    current_values = values_at(values, acc[rating])
+    counts = count_unique_values(current_values)
+    # Eventually, one side disappears, for the next condition not to fails. I ||= 0 here
+    counts["0"] ||= 0
+    counts["1"] ||= 0
+    acc[rating] = \
+      if condition.call(counts["0"], counts["1"])
+        indexes_with_value(current_values, "0")
+      else
+        indexes_with_value(current_values, "1")
+      end
+  end
+
+  def solve_one_rating(values, acc, rating, condition)
+    locate_indexes(values, acc, rating, condition) unless found_value? acc[rating]
+  end
+
+  def display_answer(result)
+    puts result.inspect
+    final = 1
+    [:oxygen, :co2].each do |rating|
+      answer = data[result[rating].last].to_i(2)
+      final *= answer
+      puts "Answer #{rating}: #{answer}"
+    end
+    puts "Day3 Part2 Answer: #{final}"
+  end
+
+  def solve_part2
+    #12.times do |i|
+    #end
+    #oxygen_generator, co2_scrubber = split_indexes(i)
+    # I am going to keep the index of numbers that are good, because at this point, I have arrays sorted by position.
+    result = bits_collector.each_with_object(oxygen: (0..-1), co2: (0..-1)) do |(pos, values), acc|
+      # puts pos.inspect
+      #debugger if pos == 10
+      puts acc.inspect
+      # this is annoying, the first time I execute it twice for nothing.
+      solve_one_rating(values, acc, :oxygen, ->(a,b) { a > b })
+      solve_one_rating(values, acc, :co2, ->(a,b) { a <= b })
+
+      acc
+    end
+    display_answer result
+  end
 end
 
-day3 :part1
+Day3Solver.new.solve_part2
