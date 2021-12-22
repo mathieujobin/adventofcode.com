cfb26ef812da9a5dd3baf1be94e758a823bd4270
Mathieu Jobin <majobin@mdsol.com>
2021-12-22 10:09:27 +0900

    fix execution with example

--- 2021/day21.rb
+++ 2021/day21.rb
@@ -31,7 +31,7 @@ class Day21Game
     a, b = input.split("\n").map { |line| line.split(" ").last }
     @players[0].cur_position = a.to_i
     @players[1].cur_position = b.to_i
-    @cur_player = 0
+    @cur_player = -1
   end
 
   def game_over?
@@ -39,22 +39,28 @@ class Day21Game
   end
 
   def compute_new_position(player, movement)
-    new_pos = (player.cur_position + movement) % 10
+    # debugger
+    new_pos = (@players[player].cur_position + movement) % 10
     @players[player].cur_position = new_pos == 0 ? 10 : new_pos
   end
 
   def play
     until game_over?
       @cur_player = (@cur_player + 1) % 2
-      three_dices = [@die.roll, @die.roll, @die.roll].sum
-      compute_new_position(@cur_player, three_dices)
-      @players[@cur_player].score += @players[1].cur_position
+      three_dices = [@die.roll, @die.roll, @die.roll]
+      compute_new_position(@cur_player, three_dices.sum)
+      @players[@cur_player].score += @players[@cur_player].cur_position
+      puts "Player #{@cur_player + 1} rolls #{three_dices} and moves to space #{@players[@cur_player].cur_position} for a total score of #{@players[@cur_player].score}"
     end
+    puts @players.inspect
   end
 end
 
+example_input = "Player 1 starting position: 4
+Player 2 starting position: 8
+"
 part1_input = "Player 1 starting position: 1
 Player 2 starting position: 5
 "
 
-Day21Game.new(part1_input).play
+Day21Game.new(example_input).play
