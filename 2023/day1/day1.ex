4d2c4ea52c8a357eb76cb109a92105c51bea991a
Mathieu Jobin <majobin@mdsol.com>
2023-12-18 15:39:08 +0900

    attempting something

--- 2023/day1/day1.ex
+++ 2023/day1/day1.ex
@@ -1,5 +1,37 @@
-{:ok, contents} = File.read("seed1.txt")
-contents |> String.split("\n", trim: true)
+defmodule StringUtil do
+  def calib_val(str) do
+    first_digit(str) + last_digit(str)
+  end
 
-def calib_val()
-sub("^\\D+(\\d+).*", "\\1", digs)
+  def first_digit(str) when is_binary(str) do
+    ret_digit(str)
+  end
+
+  def last_digit(str) when is_binary(str) do
+    ret_digit(String.reverse(str))
+  end
+
+  defp ret_digit(<<c :: utf8, _rest :: binary>>) when c in ?0..?9 do
+    c
+  end
+
+  defp ret_digit(<<_c :: utf8, rest :: binary>>) do
+    ret_digit(rest)
+  end
+end
+
+defmodule Mathieu do
+  def calib_val(line) do
+    first_num = String.sub("^\\D+(\\d+).*", "\\1", line)
+    last_num = String.reverse(line) |> String.sub("^\\D+(\\d+).*", "\\1")
+    first_num + last_num
+  end
+end
+
+{:ok, contents} = File.read("seed1.txt")
+lines = String.split(contents, "\n", trim: true)
+values = Enum.map(lines, &StringUtil.calib_val/1)
+# sum = contents |>  |>  |>
+IO.inspect(StringUtil.last_digit("asdfghjkfvjr28vw2893asdf7ghjkl"))
+
+IO.puts Enum.sum(values)
