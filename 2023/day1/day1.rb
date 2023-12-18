16ffbebf42024e4b56e942c019ccbfaa88d42925
Mathieu Jobin <mathieu@justbudget.com>
2023-12-18 20:58:29 +0900

    replace number words with digits

--- 2023/day1/day1.rb
+++ 2023/day1/day1.rb
@@ -2,13 +2,16 @@ require 'byebug'
 
 contents = File.read("day1-seed1.txt").split("\n").compact
 
+Numbers = %w[one two three four five six seven eight nine]
+
 def calib_val(line)
-    puts line
-    #debugger if line == "eightfour538"
+    puts "before> #{line}"
+    Numbers.each_with_index { |num,i| line.gsub!(num, (i+1).to_s) }
+    puts "after>  #{line}"
     first_number = line.match(/\d+/)[0][0].to_i
     last_number = line.reverse.match(/\d+/)[0][0].to_i
     answer = (10*first_number) + last_number
-    puts answer
+    puts "answer> #{answer}"
     answer
 end
 
