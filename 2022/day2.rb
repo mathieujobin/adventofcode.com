dd612b81a83a9592119a8ec4bf450eab90fdb3d6
Mathieu Jobin <mathieu@justbudget.com>
2023-12-28 11:47:59 +0900

    left score is 13227, right score 10816

--- 2022/day2.rb
+++ 2022/day2.rb
@@ -6,31 +6,40 @@ rock = %w[A X]
 paper = %w[B Y]
 scissors = %w[C Z]
 
-score = 0
+left_score = right_score = 0
 File.readlines('day2-input.txt').each do |line|
   case line.chomp
   when 'A X'
-    score += 1 + 3
+    left_score += 1 + 3
+    right_score += 1 + 3
   when 'A Y'
-    score += 1 + 0
+    left_score += 1 + 0
+    right_score += 2 + 6
   when 'A Z'
-    score += 1 + 6
+    left_score += 1 + 6
+    right_score += 3 + 0
   when 'B X'
-    score += 2 + 6
+    left_score += 2 + 6
+    right_score += 1 + 0
   when 'B Y'
-    score += 2 + 3
+    left_score += 2 + 3
+    right_score += 2 + 3
   when 'B Z'
-    score += 2
+    left_score += 2
+    right_score += 3 + 6
   when 'C X'
-    score += 3
+    left_score += 3
+    right_score += 1 + 6
   when 'C Y'
-    score += 3+6
+    left_score += 3+6
+    right_score += 2
   when 'C Z'
-    score += 6 # 3 + 3
+    left_score += 6 # 3 + 3
+    right_score += 3 + 3
   else
     puts "error: invalid input (#{line.chomp})"
   end
 end
 
-puts score
+puts right_score
 
