b359eb5a59cc383772f59693e69d15ca0f4d12c7
Mathieu Jobin <mathieu@justbudget.com>
2023-12-28 12:12:46 +0900

    complete 2022, day 3 part 1: 7746

--- 2022/day3.rb
+++ 2022/day3.rb
@@ -8,13 +8,70 @@ def present_in_both(left, right)
   left & right
 end
 
+Score = {
+  'a' => 1,
+  'b' => 2,
+  'c' => 3,
+  'd' => 4,
+  'e' => 5,
+  'f' => 6,
+  'g' => 7,
+  'h' => 8,
+  'i' => 9,
+  'j' => 10,
+  'k' => 11,
+  'l' => 12,
+  'm' => 13,
+  'n' => 14,
+  'o' => 15,
+  'p' => 16,
+  'q' => 17,
+  'r' => 18,
+  's' => 19,
+  't' => 20,
+  'u' => 21,
+  'v' => 22,
+  'w' => 23,
+  'x' => 24,
+  'y' => 25,
+  'z' => 26,
+  'A' => 27,
+  'B' => 28,
+  'C' => 29,
+  'D' => 30,
+  'E' => 31,
+  'F' => 32,
+  'G' => 33,
+  'H' => 34,
+  'I' => 35,
+  'J' => 36,
+  'K' => 37,
+  'L' => 38,
+  'M' => 39,
+  'N' => 40,
+  'O' => 41,
+  'P' => 42,
+  'Q' => 43,
+  'R' => 44,
+  'S' => 45,
+  'T' => 46,
+  'U' => 47,
+  'V' => 48,
+  'W' => 49,
+  'X' => 50,
+  'Y' => 51,
+  'Z' => 52,
+
+}
 def part1
   data = File.readlines('day3-input.txt').map(&:chomp)
   data.map do |line|
     left, right = split_equal_length(line)
-    #puts (Array(left) && right).size
-    puts present_in_both(left, right)
-   end
+    value = present_in_both(left, right).first
+    x = Score[value] || 0
+    # puts x
+    x
+  end.sum
 end
 
-part1
+puts part1
